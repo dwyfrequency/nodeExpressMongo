ccb73612e1c0b07d80d7a688f3641a758c7de963
dwyfrequency <jdwy215@gmail.com>
2018-07-24 07:02:51 -0400

    adding initial project files

---
+++ vidjot/app.js
@@ -0,0 +1,23 @@
+// nodemon - lets us save and restart our page without rebooting our app file to see changes
+const express = require("express");
+const log = console.log;
+
+const app = express();
+
+// Index Route
+app.get('/', (req, res) => {
+  // first param - endpoint, second param callback - for request and response 
+  res.send('INDEX'); // now when we call the endpoint, we get this text in the browser
+});
+
+app.get('/about', (req, res) => {
+  // first param - endpoint, second param callback - for request and response 
+  res.send('ABOUT1'); // now when we call the endpoint, we get this text in the browser
+});
+
+const port = 5000;
+
+// starts server on port 5000
+app.listen(port, () => {
+  log(`Server started on port ${port}`);
+});
