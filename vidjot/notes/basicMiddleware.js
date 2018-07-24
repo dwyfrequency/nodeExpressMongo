13f8b431a0333c080a9215dddc126b92f522ee68
dwyfrequency <jdwy215@gmail.com>
2018-07-24 07:40:26 -0400

    adding notes one middleware

--- vidjot/notes/basicMiddleware.js
+++ vidjot/notes/basicMiddleware.js
@@ -4,6 +4,13 @@ const log = console.log;
 
 const app = express();
 
+// How middleware works - for some reason only intermediately works for me
+app.use(function (req, res, next) {
+  log(Date.now()); // logs time for each endpoint call
+  req.name = 'Mr. Decodes' // once we add this value to our request obj, we have access to it throughout the application
+  next(); // calls next piece of middleware to run
+});
+
 // Index Route
 app.get('/', (req, res) => {
   // first param - endpoint, second param callback - for request and response 
@@ -13,6 +20,7 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   // first param - endpoint, second param callback - for request and response 
   log(req.name);
+  res.send(req.name); // now when we call the endpoint, we get this text in the browser
 });
 
 const port = 5000;
